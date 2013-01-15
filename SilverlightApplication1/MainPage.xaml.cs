805633650ce3e2f6f1864525e37f95245de3925f
lesnikovskiy <lesnikovski@gmail.com>
2013-01-15 23:35:59 +0200

    silverlight learning

---
+++ SilverlightApplication1/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace SilverlightBanking
+{
+    public partial class MainPage : UserControl
+    {
+        private Owner _owner;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            InitializeOwner();
+            OwnerDetailsGrid.DataContext = _owner;
+        }
+
+        private void InitializeOwner()
+        {
+            _owner = new Owner
+                {
+                    OwnerId = 1234567,
+                    FirstName = "John",
+                    LastName = "Smith",
+                    Address = "Oxford Street 24",
+                    ZipCode = "W1A",
+                    Country = "United Kingdom",
+                    State = "NA",
+                    ImageName = new Uri("/CustomerImages/man.jpg", UriKind.Relative),
+                    LastActivityDate = DateTime.Today,
+                    CurrentBalance = 1234.56,
+                    BirthDate = new DateTime(1953, 6, 9),
+                    CustomerSince = new DateTime(1999, 12, 20),
+                    LastActivityAmount = 100
+                };
+        }
+    }
+}
---
+++ SilverlightApplication1/Owner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SilverlightBanking
+{
+    public class Owner
+    {
+        public int OwnerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime CustomerSince { get; set; }
+        public Uri ImageName { get; set; }
+        public DateTime LastActivityDate { get; set; }
+        public double CurrentBalance { get; set; }
+        public double LastActivityAmount { get; set; }
+    }
+}
